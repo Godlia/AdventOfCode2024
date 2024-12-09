94b7eec7410e3c27c39dded37d7ef01a095b6fc5
Eirik Helgerud Kristoffersen <eirik.h.kristoffersen@gmail.com>
2024-12-09 11:36:20 +0100

    day 3 part 2

--- Day3.py
+++ Day3.py
@@ -1,22 +1,41 @@
 import re
-regexExp = 'mul\([0-9]{1,3},[0-9]{1,3}\)'
+regexExp = "mul\([0-9]{1,3},[0-9]{1,3}\)|don't\(\)|do\(\)"
+
 
 def extract(string):
-    string = str.replace(string, "mul(", "")
-    string = str.replace(string, ")", "")
-    
     tempArr = []
-    for item in string.split(","):
-        tempArr.append(int(item))
+    if "don't()" in string:
+        tempArr.append(False)
+    elif "do()" in string:
+        tempArr.append(True)
+    else:
+        #if mul( ) instruction
+        string = str.replace(string, "mul(", "")
+        string = str.replace(string, ")", "")
+        for item in string.split(","):
+            tempArr.append(int(item))
     
-    
+    # print(tempArr)
     return tempArr
 
 
 with open("./Inputs/day3.txt") as file:
     ans = re.findall(regexExp, file.read())
     sum = 0
+    run = True
     for item in ans:
-        ya = extract(item)
-        sum += ya[0] * ya[1]
+        extracted = extract(item)
+        if type(extracted[0]).__name__ == "bool":
+            if extracted[0] == True:
+                run = True
+                print("Start adding")
+            elif extracted[0] == False:
+                run = False
+                print("Skips")
+        else:
+            if run == True:
+                print("Adding: ", extracted)
+                sum += extracted[0] * extracted[1]
+            else:
+                print("Skipping: ", extracted)
     print(sum)
